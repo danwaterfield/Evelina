de8355c267cf046474c6aa2733a8006b42184bbe
Jacob Weinbren <jacobweinbren@gmail.com>
2019-03-17 19:14:39 +0000

    file upload complete

--- site.py
+++ site.py
@@ -14,21 +14,20 @@ app.config['SECRET_KEY'] = SECRET_KEY
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    if request.method == 'GET':
-		return render_template('index.html', last_updated=str(int(round(time.time() * 1000))))
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	return render_template('index.html', last_updated=str(int(round(time.time() * 1000))))
 
 if __name__ == "__main__":
 	app.run(debug=True)
